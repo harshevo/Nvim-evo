fae0be38b61e6e28e66a6c4cf09e4405e8602fef
Harsh Mishra <hkmmishra18@gmail.com>
2024-01-04 22:09:33 +0530

    changed colorscheme catppuccin

--- init.lua
+++ init.lua
@@ -193,11 +193,54 @@ require('lazy').setup({
 
   {
     -- Theme inspired by Atom
-    'navarasu/onedark.nvim',
-    priority = 1000,
-    config = function()
-      vim.cmd.colorscheme 'onedark'
-    end,
+    --'navarasu/onedark.nvim',
+    --priority = 1000,
+    --config = function()
+      --vim.cmd.colorscheme 'onedark'
+    --end,
+    "catppuccin/nvim",
+        name = "catppuccin",
+        config = function()
+            require("catppuccin").setup {
+                flavour = "mocha", -- latte, frappe, macchiato, mocha
+                term_colors = true,
+                transparent_background = true,
+                no_italic = false,
+                no_bold = false,
+                styles = {
+                    comments = {},
+                    conditionals = {},
+                    loops = {},
+                    functions = {},
+                    keywords = {},
+                    strings = {},
+                    variables = {},
+                    numbers = {},
+                    booleans = {},
+                    properties = {},
+                    types = {},
+                },
+                color_overrides = {
+                    mocha = {
+                        base = "#000000",
+                        mantle = "#000000",
+                        crust = "#000000",
+                    },
+                },
+                highlight_overrides = {
+                    mocha = function(C)
+                        return {
+                            TabLineSel = { bg = C.pink },
+                            CmpBorder = { fg = C.surface2 },
+                            Pmenu = { bg = C.none },
+                            TelescopeBorder = { link = "FloatBorder" },
+                        }
+                    end,
+                },
+            }
+
+            vim.cmd.colorscheme "catppuccin"
+        end,
   },
 
   {
