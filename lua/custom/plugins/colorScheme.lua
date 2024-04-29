8dc087a18ade95502d008a670a964cde81fd2807
harsh <hkmmishra18@gmail.com>
2024-04-29 10:32:43 +0530

    code format

--- lua/custom/plugins/colorScheme.lua
+++ lua/custom/plugins/colorScheme.lua
@@ -9,53 +9,52 @@ return {
   -- end,
 
   -- 2nd
-  -- "tiagovla/tokyodark.nvim",
-  -- opts = {
-  -- },
+  -- 'tiagovla/tokyodark.nvim',
+  -- opts = {},
   -- config = function(_, opts)
-  --     require("tokyodark").setup(opts) -- calling setup is optional
-  --     vim.cmd [[colorscheme tokyodark]]
+  --   require('tokyodark').setup(opts) -- calling setup is optional
+  --   vim.cmd [[colorscheme tokyodark]]
   -- end,
   -- config = function()
-  --     require("catppuccin").setup {
-  --         flavour = "mocha", -- latte, frappe, macchiato, mocha
-  --         term_colors = true,
-  --         transparent_background = false,
-  --         no_italic = false,
-  --         no_bold = false,
-  --         styles = {
-  --             comments = {},
-  --             conditionals = {},
-  --             loops = {},
-  --             functions = {},
-  --             keywords = {},
-  --             strings = {},
-  --             variables = {},
-  --             numbers = {},
-  --             booleans = {},
-  --             properties = {},
-  --             types = {},
-  --         },
-  --         color_overrides = {
-  --             mocha = {
-  --                 base = "#000000",
-  --                 mantle = "#000000",
-  --                 crust = "#000000",
-  --             },
-  --         },
-  --         highlight_overrides = {
-  --             mocha = function(C)
-  --                 return {
-  --                     TabLineSel = { bg = C.pink },
-  --                     CmpBorder = { fg = C.surface2 },
-  --                     Pmenu = { bg = C.none },
-  --                     TelescopeBorder = { link = "FloatBorder" },
-  --                 }
-  --             end,
-  --         },
-  --     }
+  --   require('catppuccin').setup {
+  --     flavour = 'mocha', -- latte, frappe, macchiato, mocha
+  --     term_colors = true,
+  --     transparent_background = false,
+  --     no_italic = false,
+  --     no_bold = false,
+  --     styles = {
+  --       comments = {},
+  --       conditionals = {},
+  --       loops = {},
+  --       functions = {},
+  --       keywords = {},
+  --       strings = {},
+  --       variables = {},
+  --       numbers = {},
+  --       booleans = {},
+  --       properties = {},
+  --       types = {},
+  --     },
+  --     color_overrides = {
+  --       mocha = {
+  --         base = '#000000',
+  --         mantle = '#000000',
+  --         crust = '#000000',
+  --       },
+  --     },
+  --     highlight_overrides = {
+  --       mocha = function(C)
+  --         return {
+  --           TabLineSel = { bg = C.pink },
+  --           CmpBorder = { fg = C.surface2 },
+  --           Pmenu = { bg = C.none },
+  --           TelescopeBorder = { link = 'FloatBorder' },
+  --         }
+  --       end,
+  --     },
+  --   }
   --
-  --     vim.cmd.colorscheme "catppuccin"
+  --   vim.cmd.colorscheme 'catppuccin'
   -- end,
   --
 
@@ -74,8 +73,6 @@ return {
   --   vim.cmd 'colorscheme tokyonight-night'
   -- end,
 
-  --
-  --
   -- 4th
   -- 'EdenEast/nightfox.nvim',
   -- lazy = false,
@@ -83,7 +80,7 @@ return {
   -- config = function()
   --   require('nightfox').setup {
   --     options = {
-  --       transparent = true,
+  --       transparent = false,
   --     },
   --   }
   --   vim.cmd.colorscheme 'carbonfox'
