87ac3fcf2b0d1207da8b3e4db9bbf7319179d367
harsh <hkmmishra18@gmail.com>
2024-05-17 22:57:35 +0530

    downgraded indent-blankline

--- init.lua
+++ init.lua
@@ -47,6 +47,8 @@ require('lazy').setup({
   {
     'lukas-reineke/indent-blankline.nvim',
     main = 'ibl',
+    commit = '3d08501caef2329aba5121b753e903904088f7e6', -- release v3.5.4
+    dependencies = 'nvim-treesitter/nvim-treesitter',
     opts = {},
   },
 
