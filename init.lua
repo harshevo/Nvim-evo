edc853e759f14c168e5cdfebe331bead7eb88e5d
harsh <hkmmishra18@gmail.com>
2024-05-18 04:09:50 +0530

    removed indents

--- init.lua
+++ init.lua
@@ -44,13 +44,13 @@ require('lazy').setup({
     },
   },
 
-  {
-    'lukas-reineke/indent-blankline.nvim',
-    main = 'ibl',
-    commit = '3d08501caef2329aba5121b753e903904088f7e6', -- release v3.5.4
-    dependencies = 'nvim-treesitter/nvim-treesitter',
-    opts = {},
-  },
+  -- {
+  --   'lukas-reineke/indent-blankline.nvim',
+  --   main = 'ibl',
+  --   commit = '3d08501caef2329aba5121b753e903904088f7e6', -- release v3.5.4
+  --   dependencies = 'nvim-treesitter/nvim-treesitter',
+  --   opts = {},
+  -- },
 
   -- Useful plugin to show you pending keybinds.
   { 'folke/which-key.nvim', opts = {} },
