baa012e919005c09c37c96e6d89ffa3536159b79
harsh <hkmmishra18@gmail.com>
2024-05-31 23:11:17 +0530

    update

---
+++ lua/custom/plugins/bufferline.lua
@@ -0,0 +1,11 @@
+return {
+  'akinsho/bufferline.nvim',
+  version = '*',
+  dependencies = 'nvim-tree/nvim-web-devicons',
+  opts = {
+    options = {
+      mode = 'buffer',
+      separator_style = 'slant',
+    },
+  },
+}
