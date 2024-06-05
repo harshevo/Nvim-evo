f75a2781d99a690a8cd4da93149e777cbb27f2bd
harsh <hkmmishra18@gmail.com>
2024-06-06 05:14:15 +0530

    kickstart folder removed

--- lua/custom/plugins/bufferline.lua
+++ lua/custom/plugins/bufferline.lua
@@ -5,7 +5,7 @@ return {
   opts = {
     options = {
       mode = 'buffer',
-      separator_style = 'slant',
+      separator_style = '',
     },
   },
 }
--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -5,7 +5,7 @@ return {
   opts = {
     options = {
       icons_enabled = true,
-      theme = 'tokyonight',
+      -- theme = 'tokyonight',
       component_separators = '|',
       section_separators = '',
     },
