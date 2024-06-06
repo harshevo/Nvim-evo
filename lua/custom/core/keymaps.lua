24ec701424d4a156a9650a6982ea6fd425ed61b0
harsh <hkmmishra18@gmail.com>
2024-06-06 07:23:51 +0530

    modularised files

--- lua/custom/core/keymaps.lua
+++ lua/custom/core/keymaps.lua
@@ -33,3 +33,32 @@ vim.keymap.set('n', '<leader>sv', '<C-w>v', { desc = 'Split window vertically' }
 -- vim.keymap.set('n', '<leader>sh', '<C-w>s', { desc = 'Split window vertically' })
 -- vim.keymap.set('n', '<leader>se', '<C-w>=', { desc = 'Split window vertically' })
 vim.keymap.set('n', '<leader>sx', '<cmd>close<CR>', { desc = 'Split window vertically' })
+
+---------------------------------------------------------------------------
+-- Keymaps for better default experience
+-- See `:help vim.keymap.set()`
+vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+
+-- Remap for dealing with word wrap
+vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+
+-- Diagnostic keymaps
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
+vim.keymap.set('n', '<leader>q', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
+-- vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
+
+vim.api.nvim_create_user_command('DiagnosticToggle', function()
+  local config = vim.diagnostic.config
+  local vt = config().virtual_text
+  config {
+    virtual_text = not vt,
+    underline = not vt,
+    signs = not vt,
+  }
+end, { desc = 'toggle diagnostic' })
+
+vim.keymap.set('n', '<leader><S-e>', function()
+  vim.cmd.DiagnosticToggle()
+end)
---
+++ lua/custom/lazy.lua
@@ -0,0 +1,29 @@
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system {
+		'git',
+		'clone',
+		'--filter=blob:none',
+		'https://github.com/folke/lazy.nvim.git',
+		'--branch=stable', -- latest stable release
+		lazypath,
+	}
+end
+vim.opt.rtp:prepend(lazypath)
+
+require('lazy').setup({
+	-- NOTE: First, some plugins that don't require any configuration
+
+	{ import = 'custom.plugins' },
+	{import = 'custom.plugins.lsp'},
+
+	-- Git related plugins
+	'tpope/vim-fugitive',
+	'tpope/vim-rhubarb',
+
+	-- Detect tabstop and shiftwidth automatically
+	'tpope/vim-sleuth',
+
+	-- NOTE: This is where your plugins related to LSP can be installed.
+	--  The configuration is done below. Search for lspconfig to find it below.
+}, {})
