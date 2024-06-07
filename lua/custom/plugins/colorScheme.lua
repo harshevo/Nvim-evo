125c480deda3538b7c81f276d7049c9cf7b2fe07
harsh <hkmmishra18@gmail.com>
2024-06-07 20:19:39 +0530

    changed colorscheme

--- lua/custom/plugins/colorScheme.lua
+++ lua/custom/plugins/colorScheme.lua
@@ -1,144 +1,33 @@
--- Theme
 return {
-  -- 1st
-  -- Theme inspired by Atom
-  -- 'olimorris/onedarkpro.nvim',
-  -- priority = 1000,
-  -- config = function()
-  --   vim.cmd.colorscheme 'onedark_dark'
-  -- end,
-
-  -- 2nd
-  -- 'tiagovla/tokyodark.nvim',
-  -- opts = {},
-  -- config = function(_, opts)
-  --   require('tokyodark').setup(opts) -- calling setup is optional
-  --   vim.cmd [[colorscheme tokyodark]]
-  -- end,
-  -- config = function()
-  --   require('catppuccin').setup {
-  --     flavour = 'mocha', -- latte, frappe, macchiato, mocha
-  --     term_colors = true,
-  --     transparent_background = false,
-  --     no_italic = false,
-  --     no_bold = false,
-  --     styles = {
-  --       comments = {},
-  --       conditionals = {},
-  --       loops = {},
-  --       functions = {},
-  --       keywords = {},
-  --       strings = {},
-  --       variables = {},
-  --       numbers = {},
-  --       booleans = {},
-  --       properties = {},
-  --       types = {},
-  --     },
-  --     color_overrides = {
-  --       mocha = {
-  --         base = '#000000',
-  --         mantle = '#000000',
-  --         crust = '#000000',
-  --       },
-  --     },
-  --     highlight_overrides = {
-  --       mocha = function(C)
-  --         return {
-  --           TabLineSel = { bg = C.pink },
-  --           CmpBorder = { fg = C.surface2 },
-  --           Pmenu = { bg = C.none },
-  --           TelescopeBorder = { link = 'FloatBorder' },
-  --         }
-  --       end,
-  --     },
-  --   }
-  --
-  --   vim.cmd.colorscheme 'catppuccin'
-  -- end,
-  --
-
   -- --3rd
-  {
-    'folke/tokyonight.nvim',
-    lazy = false,
-    priority = 1000,
-    config = function()
-      local bg = '#19181A'
-      local bg_dark = '#19181A'
-      local bg_sidebar = '#00000F'
-      local border = '#19181E'
-      require('tokyonight').setup {
-        style = 'moon',
-        transparent = false,
-        terminal_colors = true,
-        styles = {
-          comments = { italic = true },
-        },
-        on_colors = function(colors)
-          colors.bg = bg
-          colors.bg_dark = bg_dark
-          colors.bg_popup = bg_dark
-          colors.bg_float = border
-          colors.bg_sidebar = bg_sidebar
-        end,
-      }
-
-      vim.cmd 'colorscheme tokyonight-night'
-    end,
-  },
-
-  -- 4th
-  -- 'EdenEast/nightfox.nvim',
-  -- lazy = false,
-  -- priority = 1000,
-  -- config = function()
-  --   require('nightfox').setup {
-  --     options = {
+  -- {
+  --   'folke/tokyonight.nvim',
+  --   lazy = false,
+  --   priority = 1000,
+  --   config = function()
+  --     local bg = '#19181A'
+  --     local bg_dark = '#19181A'
+  --     local bg_sidebar = '#00000F'
+  --     local border = '#19181E'
+  --     require('tokyonight').setup {
+  --       style = 'moon',
   --       transparent = false,
-  --     },
-  --   }
-  --   vim.cmd.colorscheme 'carbonfox'
-  -- end,
-
-  --5th
-  -- 'folke/tokyonight.nvim',
-  -- priority = 1000,
-  -- config = function()
-  --   local bg = '#011628'
-  --   local bg_dark = '#011423'
-  --   local bg_highlight = '#143652'
-  --   local bg_search = '#0A64AC'
-  --   local bg_visual = '#275378'
-  --   local fg = '#CBE0F0'
-  --   local fg_dark = '#B4D0E9'
-  --   local fg_gutter = '#627E97'
-  --   local border = '#547998'
+  --       terminal_colors = true,
+  --       styles = {
+  --         comments = { italic = true },
+  --       },
+  --       on_colors = function(colors)
+  --         colors.bg = bg
+  --         colors.bg_dark = bg_dark
+  --         colors.bg_popup = bg_dark
+  --         colors.bg_float = border
+  --         colors.bg_sidebar = bg_sidebar
+  --       end,
+  --     }
   --
-  --   require('tokyonight').setup {
-  --     style = 'night',
-  --     -- transparent = true,
-  --     on_colors = function(colors)
-  --       colors.bg = bg
-  --       colors.bg_dark = bg_dark
-  --       colors.bg_float = bg_dark
-  --       colors.bg_highlight = bg_highlight
-  --       colors.bg_popup = bg_dark
-  --       colors.bg_search = bg_search
-  --       colors.bg_sidebar = bg_dark
-  --       colors.bg_statusline = bg_dark
-  --       colors.bg_visual = bg_visual
-  --       colors.border = border
-  --       colors.fg = fg
-  --       colors.fg_dark = fg_dark
-  --       colors.fg_float = fg
-  --       colors.fg_gutter = fg_gutter
-  --       colors.fg_sidebar = fg_dark
-  --     end,
-  --   }
-  --
-  --   vim.cmd 'colorscheme tokyonight-night'
-  -- end,
+  --     vim.cmd 'colorscheme tokyonight-night'
+  --   end,
+  -- },
 
   -- {
   --   'tjdevries/colorbuddy.nvim',
@@ -148,9 +37,95 @@ return {
   --   end,
   -- },
 
-  -- 'RRethy/base16-nvim',
-  -- lazy = false,
-  -- config = function()
-  --   vim.cmd.colorscheme 'base16-tokyo-night-terminal-dark'
-  -- end,
+  {
+    'catppuccin/nvim',
+    name = 'catppuccin',
+    priority = 1000,
+    config = function()
+      require('catppuccin').setup {
+        flavour = 'auto', -- latte, frappe, macchiato, mocha
+        background = { -- :h background
+          light = 'latte',
+          dark = 'mocha',
+        },
+        transparent_background = false, -- disables setting the background color.
+        show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
+        term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
+        dim_inactive = {
+          enabled = false, -- dims the background color of inactive window
+          shade = 'dark',
+          percentage = 0.15, -- percentage of the shade to apply to the inactive window
+        },
+        no_italic = false, -- Force no italic
+        no_bold = false, -- Force no bold
+        no_underline = false, -- Force no underline
+        styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
+          comments = { 'italic' }, -- Change the style of comments
+          -- conditionals = { 'italic' },
+          loops = {},
+          functions = {},
+          keywords = {},
+          strings = {},
+          variables = {},
+          numbers = {},
+          booleans = {},
+          properties = {},
+          types = {},
+          operators = {},
+          -- miscs = {}, -- Uncomment to turn off hard-coded styles
+        },
+        color_overrides = {},
+        custom_highlights = {},
+        default_integrations = true,
+        integrations = {
+          cmp = true,
+          gitsigns = true,
+          nvimtree = true,
+          treesitter = true,
+          notify = false,
+          mini = {
+            enabled = true,
+            indentscope_color = '',
+          },
+        },
+      }
+      vim.cmd.colorscheme 'catppuccin'
+    end,
+  },
+
+  -- {
+  --   'rebelot/kanagawa.nvim',
+  --   lazy = false,
+  --   priority = 1000,
+  --   config = function()
+  --     -- Default options:
+  --     require('kanagawa').setup {
+  --       compile = true, -- enable compiling the colorscheme
+  --       undercurl = true, -- enable undercurls
+  --       commentStyle = { italic = true },
+  --       functionStyle = {},
+  --       keywordStyle = { italic = true },
+  --       statementStyle = { bold = true },
+  --       typeStyle = {},
+  --       transparent = false, -- do not set background color
+  --       dimInactive = false, -- dim inactive window `:h hl-NormalNC`
+  --       terminalColors = true, -- define vim.g.terminal_color_{0,17}
+  --       colors = { -- add/modify theme and palette colors
+  --         palette = {},
+  --         theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+  --       },
+  --       overrides = function(colors) -- add/modify highlights
+  --         return {}
+  --       end,
+  --       theme = 'wave', -- Load "wave" theme when 'background' option is not set
+  --       background = { -- map the value of 'background' option to a theme
+  --         dark = 'wave', -- try "dragon" !
+  --         light = 'lotus',
+  --       },
+  --     }
+  --
+  --     -- setup must be called before loading
+  --     vim.cmd 'colorscheme kanagawa'
+  --   end,
+  -- },
 }
--- lua/custom/plugins/telescope.lua
+++ lua/custom/plugins/telescope.lua
@@ -51,8 +51,7 @@ return {
       end
 
       -- Find the Git root directory from the current file's path
-      local git_root = vim.fn.systemlist('git -C ' .. vim.fn.escape(current_dir, ' ') .. ' rev-parse --show-toplevel')
-      [1]
+      local git_root = vim.fn.systemlist('git -C ' .. vim.fn.escape(current_dir, ' ') .. ' rev-parse --show-toplevel')[1]
       if vim.v.shell_error ~= 0 then
         print 'Not a git repository. Searching on current working directory'
         return cwd
