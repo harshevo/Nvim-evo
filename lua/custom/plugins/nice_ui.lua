3b07693947404716fa3e7e4c1d292567f6877639
harsh <hkmmishra18@gmail.com>
2024-06-07 12:41:49 +0530

    noice.nviom

---
+++ lua/custom/plugins/nice_ui.lua
@@ -0,0 +1,12 @@
+return -- lazy.nvim
+{
+  'folke/noice.nvim',
+  event = 'VeryLazy',
+  opts = {
+    -- add any options here
+  },
+  dependencies = {
+    -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+    'MunifTanjim/nui.nvim',
+  },
+}
