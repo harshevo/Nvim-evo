3ee1df4c5ed994ddb53d1228fd7f9bdde031ce8a
harsh mishra <hkmmishra18@gmail.com>
2024-10-09 19:08:05 +0530

    update

--- lua/custom/plugins/nvimtree.lua
+++ lua/custom/plugins/nvimtree.lua
@@ -1,3 +1,17 @@
+-- function NvimTreeTrash()
+--   local lib = require 'nvim-tree.lib'
+--   local function on_exit(job_id, data, event)
+--     lib.refresh_tree()
+--   end
+--   local node = lib.get_node_at_cursor()
+--   if node then
+--     vim.fn.jobstart('trash ' .. node.absolute_path, {
+--       detach = true,
+--       on_exit = on_exit,
+--     })
+--   end
+-- end
+--
 return {
   'nvim-tree/nvim-tree.lua',
   version = '*',
@@ -5,6 +19,7 @@ return {
   dependencies = {
     'nvim-tree/nvim-web-devicons',
   },
+
   config = function()
     require('nvim-tree').setup {
       view = {
@@ -14,5 +29,9 @@ return {
         custom = { '^\\.git' },
       },
     }
+    --
+    -- vim.g.nvim_tree_bindings = {
+    --   { key = 'd', cb = ':lua NvimTreeTrash()<CR>' },
+    -- }
   end,
 }
