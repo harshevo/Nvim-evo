f9e5b9c9b65f1a330e38ec0a76b6535206280c93
harsh mishra <hkmmishra18@gmail.com>
2025-03-25 04:30:13 +0530

    update

--- lua/custom/plugins/colorScheme.lua
+++ lua/custom/plugins/colorScheme.lua
@@ -47,70 +47,70 @@ return {
       vim.cmd 'colorscheme tokyonight'
     end,
   },
-
   --
-  --     vim.cmd 'colorscheme tokyonight-night'
-  --   end,
-  -- },
-
-  -- {
-  --   'catppuccin/nvim',
-  --   name = 'catppuccin',
-  --   priority = 1000,
-  --   config = function()
-  --     require('catppuccin').setup {
-  --       flavour = 'auto', -- latte, frappe, macchiato, mocha
-  --       background = { -- :h background
-  --         light = 'latte',
-  --         dark = 'mocha',
-  --       },
-  --       transparent_background = false, -- disables setting the background color.
-  --       show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
-  --       term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
-  --       dim_inactive = {
-  --         enabled = false, -- dims the background color of inactive window
-  --         shade = 'dark',
-  --         percentage = 0.15, -- percentage of the shade to apply to the inactive window
-  --       },
-  --       no_italic = false, -- Force no italic
-  --       no_bold = false, -- Force no bold
-  --       no_underline = false, -- Force no underline
-  --       styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
-  --         comments = { 'italic' }, -- Change the style of comments
-  --         -- conditionals = { 'italic' },
-  --         loops = {},
-  --         functions = {},
-  --         keywords = {},
-  --         strings = {},
-  --         variables = {},
-  --         numbers = {},
-  --         booleans = {},
-  --         properties = {},
-  --         types = {},
-  --         operators = {},
-  --         -- miscs = {}, -- Uncomment to turn off hard-coded styles
-  --       },
-  --       color_overrides = {},
-  --       custom_highlights = {},
-  --       default_integrations = true,
-  --       integrations = {
-  --         telescope = true,
-  --         cmp = true,
-  --         gitsigns = true,
-  --         nvimtree = true,
-  --         treesitter = true,
-  --         notify = false,
-  --         mini = {
-  --           enabled = true,
-  --           indentscope_color = '',
-  --         },
-  --       },
-  --     }
-  --     vim.cmd.colorscheme 'catppuccin'
-  --   end,
-  -- },
+  -- --
+  -- --     vim.cmd 'colorscheme tokyonight-night'
+  -- --   end,
+  -- -- },
   --
-
+  -- -- {
+  -- --   'catppuccin/nvim',
+  -- --   name = 'catppuccin',
+  -- --   priority = 1000,
+  -- --   config = function()
+  -- --     require('catppuccin').setup {
+  -- --       flavour = 'auto', -- latte, frappe, macchiato, mocha
+  -- --       background = { -- :h background
+  -- --         light = 'latte',
+  -- --         dark = 'mocha',
+  -- --       },
+  -- --       transparent_background = false, -- disables setting the background color.
+  -- --       show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
+  -- --       term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
+  -- --       dim_inactive = {
+  -- --         enabled = false, -- dims the background color of inactive window
+  -- --         shade = 'dark',
+  -- --         percentage = 0.15, -- percentage of the shade to apply to the inactive window
+  -- --       },
+  -- --       no_italic = false, -- Force no italic
+  -- --       no_bold = false, -- Force no bold
+  -- --       no_underline = false, -- Force no underline
+  -- --       styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
+  -- --         comments = { 'italic' }, -- Change the style of comments
+  -- --         -- conditionals = { 'italic' },
+  -- --         loops = {},
+  -- --         functions = {},
+  -- --         keywords = {},
+  -- --         strings = {},
+  -- --         variables = {},
+  -- --         numbers = {},
+  -- --         booleans = {},
+  -- --         properties = {},
+  -- --         types = {},
+  -- --         operators = {},
+  -- --         -- miscs = {}, -- Uncomment to turn off hard-coded styles
+  -- --       },
+  -- --       color_overrides = {},
+  -- --       custom_highlights = {},
+  -- --       default_integrations = true,
+  -- --       integrations = {
+  -- --         telescope = true,
+  -- --         cmp = true,
+  -- --         gitsigns = true,
+  -- --         nvimtree = true,
+  -- --         treesitter = true,
+  -- --         notify = false,
+  -- --         mini = {
+  -- --           enabled = true,
+  -- --           indentscope_color = '',
+  -- --         },
+  -- --       },
+  -- --     }
+  -- --     vim.cmd.colorscheme 'catppuccin'
+  -- --   end,
+  -- -- },
+  -- --
+  --
   -- {
   --   'EdenEast/nightfox.nvim',
   --   lazy = false,
@@ -118,43 +118,43 @@ return {
   --     vim.cmd.colorscheme 'duskfox'
   --   end,
   -- },
-
-  -- {
-  --   'Everblush/nvim',
-  --   name = 'everblush',
-  --   config = function()
-  --     vim.cmd.colorscheme 'everblush'
-  --   end,
-  -- },
   --
-  -- {
-  --   'oxfist/night-owl.nvim',
-  --   lazy = false, -- make sure we load this during startup if it is your main colorscheme
-  --   priority = 1000, -- make sure to load this before all the other start plugins
-  --   config = function()
-  --     -- load the colorscheme here
-  --     require('night-owl').setup()
-  --     vim.cmd.colorscheme 'night-owl'
-  --   end,
-  -- },
-
-  -- {
-  --   'rose-pine/neovim',
-  --   name = 'rose-pine',
+  -- -- {
+  -- --   'Everblush/nvim',
+  -- --   name = 'everblush',
+  -- --   config = function()
+  -- --     vim.cmd.colorscheme 'everblush'
+  -- --   end,
+  -- -- },
+  -- --
+  -- -- {
+  -- --   'oxfist/night-owl.nvim',
+  -- --   lazy = false, -- make sure we load this during startup if it is your main colorscheme
+  -- --   priority = 1000, -- make sure to load this before all the other start plugins
+  -- --   config = function()
+  -- --     -- load the colorscheme here
+  -- --     require('night-owl').setup()
+  -- --     vim.cmd.colorscheme 'night-owl'
+  -- --   end,
+  -- -- },
   --
-  --   config = function()
-  --     require('rose-pine').setup {
-  --       variant = 'auto',
-  --
-  --       styles = {
-  --         bold = false,
-  --         italic = false,
-  --         transparency = false,
-  --       },
-  --     }
-  --     vim.cmd.colorscheme 'rose-pine'
-  --   end,
-  -- },
+  -- -- {
+  -- --   'rose-pine/neovim',
+  -- --   name = 'rose-pine',
+  -- --
+  -- --   config = function()
+  -- --     require('rose-pine').setup {
+  -- --       variant = 'auto',
+  -- --
+  -- --       styles = {
+  -- --         bold = false,
+  -- --         italic = false,
+  -- --         transparency = false,
+  -- --       },
+  -- --     }
+  -- --     vim.cmd.colorscheme 'rose-pine'
+  -- --   end,
+  -- -- },
 
   -- {
   --   'projekt0n/github-nvim-theme',
@@ -164,6 +164,16 @@ return {
   --     require('github-theme').setup {}
   --
   --     vim.cmd 'colorscheme github_dark'
+  --   end,
+  -- },
+
+  -- {
+  --   'blazkowolf/gruber-darker.nvim',
+  --   config = function()
+  --     require('gruber-darker').setup {
+  --       bold = false,
+  --     }
+  --     vim.cmd 'colorscheme gruber-darker'
   --   end,
   -- },
 }
