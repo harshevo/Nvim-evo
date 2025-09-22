e88678463fffdafca26ab8dac6cc848b1d785929
harsh mishra <hkmmishra18@gmail.com>
2025-09-23 00:50:24 +0530

    fixed colorscheme

--- lua/custom/plugins/colorScheme.lua
+++ lua/custom/plugins/colorScheme.lua
@@ -192,14 +192,11 @@ return {
 
   {
     'blazkowolf/gruber-darker.nvim',
-    opts = {
-      bold = false,
-      italic = {
-        strings = false,
-      },
-    },
 
     config = function()
+      require('gruber-darker').setup {
+        bold = false,
+      }
       vim.cmd.colorscheme 'gruber-darker'
     end,
   },
