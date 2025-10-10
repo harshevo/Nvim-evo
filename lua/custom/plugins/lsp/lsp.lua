5c8c15490f41a5ad48c6baa73964d16abbcbf8c7
harsh mishra <hkmmishra18@gmail.com>
2025-10-10 21:35:53 +0530

    fixed lsp

--- lua/custom/plugins/lsp/lsp.lua
+++ lua/custom/plugins/lsp/lsp.lua
@@ -6,59 +6,36 @@ return {
       'williamboman/mason.nvim',
       'williamboman/mason-lspconfig.nvim',
       'WhoIsSethDaniel/mason-tool-installer.nvim',
-
       { 'j-hui/fidget.nvim', opts = {} },
-
-      -- Autoformatting
       'stevearc/conform.nvim',
-
-      -- Schema information
       'b0o/SchemaStore.nvim',
     },
-    config = function()
-      require('neodev').setup {
-        -- library = {
-        --   plugins = { "nvim-dap-ui" },
-        --   types = true,
-        -- },
-      }
 
+    config = function()
+      -- === neodev: Lua runtime/LSP tweaks ===
+      require('neodev').setup {}
+
+      -- === Capabilities ===
       local capabilities = nil
       if pcall(require, 'cmp_nvim_lsp') then
         capabilities = require('cmp_nvim_lsp').default_capabilities()
       end
 
-      local lspconfig = require 'lspconfig'
-
+      -- === Language servers definition ===
       local servers = {
         bashls = true,
-        -- gopls = {
-        --   settings = {
-        --     gopls = {
-        --       hints = {
-        --         assignVariableTypes = true,
-        --         compositeLiteralFields = true,
-        --         compositeLiteralTypes = true,
-        --         constantValues = true,
-        --         functionTypeParameters = true,
-        --         parameterNames = true,
-        --         rangeVariableTypes = true,
-        --       },
-        --     },
-        --   },
-        -- },
+
         lua_ls = {
-          server_capabilities = {
-            semanticTokensProvider = vim.NIL,
+          server_capabilities = { semanticTokensProvider = vim.NIL },
+          settings = {
+            Lua = {
+              runtime = { version = 'LuaJIT' },
+              diagnostics = { globals = { 'vim' } },
+              workspace = { checkThirdParty = false },
+              telemetry = { enable = false },
+            },
           },
         },
-        -- rust_analyzer = true,
-        -- svelte = true,
-        -- templ = true,
-        -- cssls = true,
-
-        -- Probably want to disable formatting for this lang server
-        -- biome = true,
 
         jsonls = {
           settings = {
@@ -72,10 +49,7 @@ return {
         yamlls = {
           settings = {
             yaml = {
-              schemaStore = {
-                enable = false,
-                url = '',
-              },
+              schemaStore = { enable = false, url = '' },
               schemas = require('schemastore').yaml.schemas(),
             },
           },
@@ -87,20 +61,15 @@ return {
             codelens = { enable = true },
             inlayHints = { enable = true },
           },
-
           filetypes = {
             'ocaml',
             'ocaml.interface',
             'ocaml.menhir',
             'ocaml.cram',
           },
-
-          -- TODO: Check if i still need the filtypes stuff i had before
         },
 
         clangd = {
-          -- TODO: Could include cmd, but not sure those were all relevant flags.
-          --    looks like something i would have added while i was floundering
           init_options = { clangdFileStatus = true },
           filetypes = { 'c', 'cpp' },
         },
@@ -108,6 +77,9 @@ return {
         pyright = true,
         dockerls = true,
       }
+
+      -- === Mason setup ===
+      require('mason').setup()
 
       local servers_to_install = vim.tbl_filter(function(key)
         local t = servers[key]
@@ -118,40 +90,45 @@ return {
         end
       end, vim.tbl_keys(servers))
 
-      require('mason').setup()
       local ensure_installed = {
         'stylua',
         'lua_ls',
         'delve',
         'tailwindcss-language-server',
       }
-
       vim.list_extend(ensure_installed, servers_to_install)
-      require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
-      for name, config in pairs(servers) do
-        if config == true then
-          config = {}
-        end
-        config = vim.tbl_deep_extend('force', {}, {
-          capabilities = capabilities,
-        }, config)
+      require('mason-tool-installer').setup {
+        ensure_installed = ensure_installed,
+      }
+
+      -- === Register & enable servers (pure Neovim 0.11 API) ===
+      for name, cfg in pairs(servers) do
+        local cfg_table = (cfg == true) and {} or vim.deepcopy(cfg)
+        cfg_table = vim.tbl_deep_extend('force', {}, { capabilities = capabilities }, cfg_table)
 
         if name == 'tsserver' then
           name = 'ts_ls'
         end
 
-        lspconfig[name].setup(config)
+        -- Register configuration
+        pcall(function()
+          vim.lsp.config(name, cfg_table)
+        end)
+
+        -- Enable it for its filetypes
+        pcall(function()
+          vim.lsp.enable(name)
+        end)
       end
 
-      local disable_semantic_tokens = {
-        lua = true,
-      }
+      -- === LspAttach: keymaps & overrides ===
+      local disable_semantic_tokens = { lua = true }
 
       vim.api.nvim_create_autocmd('LspAttach', {
         callback = function(args)
           local bufnr = args.buf
-          local client = assert(vim.lsp.get_client_by_id(args.data.client_id), 'must have valid client')
+          local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
 
           local settings = servers[client.name]
           if type(settings) ~= 'table' then
@@ -161,47 +138,33 @@ return {
           local builtin = require 'telescope.builtin'
 
           vim.opt_local.omnifunc = 'v:lua.vim.lsp.omnifunc'
-          vim.keymap.set('n', 'gd', builtin.lsp_definitions, { buffer = 0 })
-          vim.keymap.set('n', 'gr', builtin.lsp_references, { buffer = 0 })
-          vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, { buffer = 0 })
-          vim.keymap.set('n', 'gT', vim.lsp.buf.type_definition, { buffer = 0 })
-          vim.keymap.set('n', 'K', vim.lsp.buf.hover, { buffer = 0 })
+          vim.keymap.set('n', 'gd', builtin.lsp_definitions, { buffer = bufnr })
+          vim.keymap.set('n', 'gr', builtin.lsp_references, { buffer = bufnr })
+          vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, { buffer = bufnr })
+          vim.keymap.set('n', 'gT', vim.lsp.buf.type_definition, { buffer = bufnr })
+          vim.keymap.set('n', 'K', vim.lsp.buf.hover, { buffer = bufnr })
+          vim.keymap.set('n', '<space>cr', vim.lsp.buf.rename, { buffer = bufnr })
+          vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, { buffer = bufnr })
 
-          vim.keymap.set('n', '<space>cr', vim.lsp.buf.rename, { buffer = 0 })
-          vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, { buffer = 0 })
-
-          local filetype = vim.bo[bufnr].filetype
-          if disable_semantic_tokens[filetype] then
+          if disable_semantic_tokens[vim.bo[bufnr].filetype] then
             client.server_capabilities.semanticTokensProvider = nil
           end
 
-          -- Override server capabilities
           if settings.server_capabilities then
             for k, v in pairs(settings.server_capabilities) do
-              if v == vim.NIL then
-                ---@diagnostic disable-next-line: cast-local-type
-                v = nil
-              end
-
-              client.server_capabilities[k] = v
+              client.server_capabilities[k] = (v == vim.NIL) and nil or v
             end
           end
         end,
       })
 
+      -- === Diagnostics & UI borders ===
       local _border = 'single'
+      vim.diagnostic.config { float = { border = _border } }
+      vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(vim.lsp.handlers.hover, { border = _border })
+      vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = _border })
 
-      vim.diagnostic.config {
-        float = { border = _border },
-      }
-      vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(vim.lsp.handlers.hover, {
-        border = _border,
-      })
-
-      vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(vim.lsp.handlers.signature_help, {
-        border = _border,
-      })
-
+      -- === Autoformat on save ===
       vim.api.nvim_create_autocmd('BufWritePre', {
         callback = function(args)
           require('conform').format {
